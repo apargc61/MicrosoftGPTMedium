97807456f27ee3fc0f4c885ebe9e579cb02951dd
Macbook <macbook@macbooks-mbp-2.mynetworksettings.com>
2023-07-16 18:53:19 -0700

    first commit

---
+++ steps.sh
@@ -0,0 +1,11 @@
+python -m venv forchat
+source forchat/bin/activate
+pip install torch transformers
+pip install torch
+pip install flask
+pip uninstall requests
+pip install requests==2.27.1
+pip install transformers
+pip uninstall tokenizers
+pip install tokenizers
+python app.py
