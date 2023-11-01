63d9ab58080f13e27b0ecb6526d3f1590fb5e1ec
apar <apar@apars-air.mynetworksettings.com>
2023-11-01 19:47:58 -0400

    all changes from airbook

--- KnowledgeGraph.py
+++ KnowledgeGraph.py
@@ -1,46 +1,30 @@
-import spacy
 import networkx as nx
-
-# Load a spaCy NLP model
-nlp = spacy.load("en_core_web_sm")
-
-# Example text data (you would typically load a large dataset)
-text_data = [
-    "Apple Inc. is headquartered in Cupertino, California.",
-    "Steve Jobs was a co-founder of Apple Inc.",
-    "The iPhone is a popular product by Apple.",
-    "Tesla, Inc. is known for electric cars and renewable energy solutions.",
-    "Elon Musk is the CEO of Tesla, Inc."
-]
-
-# Initialize a Knowledge Graph using NetworkX
-knowledge_graph = nx.Graph()
-
-# Process each text and extract entities and relationships
-for text in text_data:
-    doc = nlp(text)
-    print("doc", doc)
-    # Extract entities (e.g., organizations, persons)
-    entities = [ent.text for ent in doc.ents]
-    print("entitits", entities)
-    # Create nodes for entities if they don't exist in the graph
-    for entity in entities:
-        if not knowledge_graph.has_node(entity):
-            knowledge_graph.add_node(entity)
-    
-    # Extract relationships (e.g., "is headquartered in")
-    relationships = [(token.head.text, token.text) for token in doc if token.dep_ == "prep"]
-    print("relationships", relationships)
-    # Create edges for relationships in the graph
-    for relationship in relationships:
-        knowledge_graph.add_edge(relationship[0], relationship[1])
-
-# Visualize the knowledge graph (optional)
 import matplotlib.pyplot as plt
-pos = nx.spring_layout(knowledge_graph)
-nx.draw(knowledge_graph, pos, with_labels=True, node_size=800, node_color="skyblue")
-plt.show()
 
-# Query the knowledge graph
-print("Entities:", knowledge_graph.nodes())
-print("Relationships:", knowledge_graph.edges())
+def draw_knowledge_graph():
+    # Create a directed graph
+    G = nx.DiGraph()
+
+    # Add nodes (entities)
+    G.add_node("Albert Einstein")
+    G.add_node("Physics")
+    G.add_node("Germany")
+
+    # Add edges (relationships)
+    G.add_edge("Albert Einstein", "Physics", relation="worked in field of")
+    G.add_edge("Albert Einstein", "Germany", relation="born in")
+
+    # Draw the graph
+    pos = nx.spring_layout(G)
+    plt.figure(figsize=(10, 6))
+    
+    nx.draw(G, pos, with_labels=True, node_size=3000, node_color="skyblue", font_size=15, font_weight='bold', width=2, edge_color="gray")
+    
+    # Draw edge labels
+    edge_labels = {(u, v): G[u][v]['relation'] for u, v in G.edges()}
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=12)
+
+    plt.title("Simple Knowledge Graph")
+    plt.show()
+
+draw_knowledge_graph()
